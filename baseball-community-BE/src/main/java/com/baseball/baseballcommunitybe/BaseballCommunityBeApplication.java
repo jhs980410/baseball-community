ad907a1f425ac43fe12daecaafb40d55ac9aad38
jhs <skc05096@naver.com>
2025-08-21 18:40:17 +0900

    기본환경셋팅중

---
+++ baseball-community-BE/src/main/java/com/baseball/baseballcommunitybe/BaseballCommunityBeApplication.java
@@ -0,0 +1,13 @@
+package com.baseball.baseballcommunitybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballCommunityBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballCommunityBeApplication.class, args);
+    }
+
+}
---
+++ baseball-community-BE/src/test/java/com/baseball/baseballcommunitybe/BaseballCommunityBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseball.baseballcommunitybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballCommunityBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
